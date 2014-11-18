51e941cde4888d1f169efb6d98018d27cb9e7392
Mar Canet <mar.canet@gmail.com>
2014-11-19 00:55:25 +0200

    add code

---
+++ code/distanceSensor/distanceSensor.ino
@@ -0,0 +1,27 @@
+/*
+  DigitalReadSerial
+ Reads a digital input on pin 2, prints the result to the serial monitor 
+ 
+ This example code is in the public domain.
+ */
+
+// digital pin 2 has a distance sensor attached to it. Give it a name:
+int distanceSensor = 2;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(distanceSensor, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int sensorState = digitalRead(distanceSensor);
+  // print out the state of the button:
+  Serial.println(sensorState);
+  
+  delay(1);        // delay in between reads for stability
+}
